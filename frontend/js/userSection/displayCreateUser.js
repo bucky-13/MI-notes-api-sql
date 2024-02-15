7aa72b66f5675d53f53e474f4a40b29f6e5fa79a
Simon Stenström <simon.stenstrom.013@gmail.com>
2024-02-15 15:59:16 +0100

    added screen for create new user

--- frontend/js/userSection/displayCreateUser.js
+++ frontend/js/userSection/displayCreateUser.js
@@ -1,3 +1,37 @@
+import displayLogin from './displayLogin.js';
+import {
+  createH2,
+  createInputWithLabel,
+  createButton,
+  createLink,
+} from '../createElements.js';
+
+let app = document.querySelector('#app');
+
 export default function displayCreateUser() {
-  console.log('I am user');
+  app.innerHTML = '';
+  let h2 = createH2('Create new Account');
+  let nameInput = createInputWithLabel('text', 'userName', 'User name:');
+  let emailInput = createInputWithLabel('email', 'userEmail', 'Email:');
+  let passwordInput = createInputWithLabel(
+    'password',
+    'userPassword',
+    'Password:'
+  );
+  let link = createLink(
+    'goToLoginLink',
+    'Already a user? Click here to log in',
+    'margin-top-1-rem'
+  );
+  let button = createButton('submitBtn', 'Create Account', 'margin-top-1-rem');
+
+  app.append(h2, nameInput, emailInput, passwordInput, button, link);
+
+  document.querySelector('#submitBtn').addEventListener('click', () => {
+    console.log(userEmail.value, userPassword.value);
+  });
+
+  document
+    .querySelector('#goToLoginLink')
+    .addEventListener('click', displayLogin);
 }
