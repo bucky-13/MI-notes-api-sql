7141227ad25e81739399b7f8aa44e536ca4f99ba
Simon Stenström <simon.stenstrom.013@gmail.com>
2024-02-20 10:30:28 +0100

    fixed backend for updating user info and password encryption

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const CryptoJS = require('crypto-js');
 
 // recieve all users
 router.get('/', function (req, res, next) {
@@ -31,11 +32,15 @@ router.post('/', function (req, res, next) {
     let userEmail = req.body.userEmail;
     //  ADD password encryption later on
     let userPassword = req.body.userPassword;
+    let encryptedPw = CryptoJS.AES.encrypt(
+      userPassword,
+      process.env.KEY_OF_SALT
+    ).toString();
 
     let firstSql = `SELECT * FROM users WHERE userName="${userName}" OR userEmail="${userEmail}"`;
 
     // POST new user into database
-    let secondSql = `INSERT INTO users (userName, userEmail, userPassword) VALUES ("${userName}", "${userEmail}", "${userPassword}")`;
+    let secondSql = `INSERT INTO users (userName, userEmail, userPassword) VALUES ("${userName}", "${userEmail}", "${encryptedPw}")`;
 
     req.app.locals.con.query(firstSql, function (err, result) {
       if (err) {
@@ -67,8 +72,8 @@ router.post('/login', (req, res, next) => {
 
     // If a user have been soft deleted, they cannot login
 
-    let sql = `SELECT userId, userName, userEmail FROM users WHERE userEmail="${userEmail}"  AND userPassword="${userPassword}" AND deleted="0"`;
-
+    let sql = `SELECT userId, userName, userEmail, userPassword FROM users WHERE userEmail="${userEmail}"AND deleted="0"`;
+    // CHECK IF USEREMAIL EXISTS, AND GET USER INFORMATION IF IT DOES
     req.app.locals.con.query(sql, function (err, result) {
       if (err) {
         console.log(err);
@@ -76,14 +81,82 @@ router.post('/login', (req, res, next) => {
 
       if (!result[0]) {
         res.status(401).json({
-          message: 'Login failed, email or password are incorrect.',
+          message: 'Login failed, email is incorrect.',
         });
       } else {
-        let user = result[0];
-        res.json(user);
+        // CHECK IF SENT PASSWORD MATCHES PASSWORD IN DATABASE
+        let storedPassword = CryptoJS.AES.decrypt(
+          result[0].userPassword,
+          process.env.KEY_OF_SALT
+        ).toString(CryptoJS.enc.Utf8);
+        if (storedPassword === userPassword) {
+          let user = result[0];
+          delete user.userPassword;
+          res.json(user);
+        } else {
+          res.status(401).json({
+            message: 'Login failed, password is incorrect.',
+          });
+        }
       }
     });
   });
 });
 
+// UPDATE USER DETAILS
+router.post('/updateUser', (req, res, next) => {
+  req.app.locals.con.connect(function (err) {
+    if (err) {
+      console.log(err);
+    }
+    let oldPassword = req.body.oldPassword;
+    let userPassword = req.body.newPassword;
+    let userId = req.body.userId;
+    let userEmail = req.body.userEmail;
+    let userName = req.body.userName;
+    let firstSql = `SELECT userId, userPassword FROM users WHERE userId="${userId}" AND deleted="0"`;
+
+    // FIRST CHECK: SEE IF USER TYPED IN CORRECT OLD PASSWORD
+    req.app.locals.con.query(firstSql, function (err, user) {
+      if (err) {
+        console.log(err);
+      }
+
+      if (oldPassword !== user[0].userPassword) {
+        res.status(401).json({
+          message: 'Login failed, email or password are incorrect.',
+        });
+      } else {
+        let secondSql = `SELECT * FROM users WHERE NOT userId="${userId}" AND (userName="${userName}" OR userEmail="${userEmail}")`;
+
+        // SECOND CHECK: SEE IF USERNAME OR EMAIL IS ALREADY TAKEN BY ANOTHER USER
+        req.app.locals.con.query(secondSql, function (err, users) {
+          if (err) {
+            console.log(err);
+          }
+          if (users[0]) {
+            res.status(401).json({
+              message: 'User name or email already in use by another user.',
+            });
+          } else {
+            let encryptedPw = CryptoJS.AES.encrypt(
+              userPassword,
+              process.env.KEY_OF_SALT
+            ).toString();
+            console.log(encryptedPw);
+            let thirdSql = `UPDATE users SET userName="${userName}", userEmail="${userEmail}", userPassword="${encryptedPw}" WHERE userId="${userId}"`;
+
+            req.app.locals.con.query(thirdSql, function (err, result) {
+              if (err) {
+                console.log(err);
+              }
+              res.json(result);
+            });
+            // console.log('yay');
+          }
+        });
+      }
+    });
+  });
+});
 module.exports = router;
